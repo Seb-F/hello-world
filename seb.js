cbd287b22e9477e7c2d16af5bb6371696157c1bb
Seb F <30931920+Seb-F@users.noreply.github.com>
2017-09-20 11:57:00 +0100

    optimising and trying out the timer
    Timer code Testing

--- seb.js
+++ seb.js
@@ -1,5 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript"></script>
+<script type="text/javascript">
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
 
  #toggle {
@@ -12,3 +12,6 @@
             margin-right: auto;
             font-size: 10pt
           }
+    
+</link>
+</script>
